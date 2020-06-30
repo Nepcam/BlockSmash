d3f1df2591cdbc9d8680e1dcbb2dd705632563d9
Nepcam <cameron.nepe@gmail.com>
2020-06-30 12:06:03 +1200

    Player score UI

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
@@ -8,9 +9,15 @@ public class GameStatus : MonoBehaviour
     // [Range] gives us a min and max over the gameSpeed variable
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // State variables
     [SerializeField] int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,5 +28,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
